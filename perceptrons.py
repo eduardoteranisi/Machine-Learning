bbe2198c8cca9a42dc93f7a4197045d620216293
kingtera <eduardoteranisi@gmail.com>
2025-02-19 17:21:23 -0300

    refactor: perceptron with just basic python and random

--- perceptrons.py
+++ perceptrons.py
@@ -1,38 +1,109 @@
-import numpy as np
+import random
 
-def unit_step_function(x):
-    return np.where(x > 0, 1, 0);
+# Sem o limiar, a fronteira de decisão sempre passa pela origem.
+# Com o limiar, a fronteira de decisão pode se deslocar, permitindo melhor separação das categoriass.
+# No código, o limiar foi implementado como um peso extra e um -1 nas amostras, tornando-o ajustável automaticamente no treinamento.
 
-class Perceptrons:
+class Perceptron:
 
-    def __init__(self, learning_rate=0.01, n_iters=1000):
-        self.lr = learning_rate;
-        self.n_iters = n_iters;
-        self.activation_func = unit_step_function;
-        self.weights = None;
-        self.bias = None;
+	def __init__(self, amostras, saidas, taxa_aprendizado=0.1, epocas=1000, limiar=-1):
 
-    def fit(self, X, y):
-        n_samples, n_features = X.shape;
+		self.amostras = amostras
+		self.saidas = saidas
+		self.taxa_aprendizado = taxa_aprendizado 
+		self.epocas = epocas 
+		self.limiar = limiar 
+		self.total_amostras = len(amostras)
+		self.elementos_amostra = len(amostras[0])
+		self.pesos = [] 
 
-        #initializing weights and bias as 0 for simplicity
-        self.weights = np.zeros(n_features);
-        self.bias = 0;
 
-        y_ = np.where(y > 0, 1, 0);
+	def treinar(self):
+		
+		# adiciona -1 para cada uma das amostras
+		for amostra in self.amostras:
+			amostra.insert(0, -1)
 
-        for _ in range(self.n_iters):
-            for idx, x_i in enumerate(X):
-                linear_output = np.dot(x_i, self.weights) + self.bias;
-                y_predicted = self.activation_func(linear_output);
+		for i in range(self.elementos_amostra):
+			self.pesos.append(random.random())
 
-                update = self.lr = (y_[idx] - y_predicted);
-                self.weights += update - x_i;
-                self.bias += update;
-    
-    def predict(self, X):
-        linear_output = np.dot(X, self.weights) + self.bias;
-        y_predicted = self.activation_func(linear_output);
-        return y_predicted;
+		# insere o limiar no vetor de pesos
+		self.pesos.insert(0, self.limiar)
 
-# Testing
+		num_epocas = 0
+
+		while True:
+
+			erro = False
+
+			for i in range(self.total_amostras):
+
+				u = 0
+
+				for j in range(self.elementos_amostra + 1):
+					u += self.pesos[j] * self.amostras[i][j]
+
+				# saída da rede pela função de ativação
+				y = self.sinal(u)
+
+				if y != self.saidas[i]:
+
+					# subtração entre a saída desejada e a saída da rede
+					erro_aux = self.saidas[i] - y
+
+					# faz o ajuste dos pesos para cada elemento da amostra
+					for j in range(self.elementos_amostra + 1):
+						self.pesos[j] = self.pesos[j] + self.taxa_aprendizado * erro_aux * self.amostras[i][j]
+
+					erro = True
+
+			num_epocas += 1
+
+			if num_epocas > self.epocas or not erro:
+				break
+
+
+	# Func teste
+	def testar(self, amostra, categorias1, categorias2):
+
+		amostra.insert(0, -1)
+
+		# utiliza o vetor de pesos que foi ajustado na fase de treinamento
+		u = 0
+		for i in range(self.elementos_amostra + 1):
+			u += self.pesos[i] * amostra[i]
+
+		# saída da rede
+		y = self.sinal(u)
+
+		# verifica a qual categorias pertence
+		if y == -1:
+			print('A amostra pertence a categorias %s' % categorias1)
+		else:
+			print('A amostra pertence a categorias %s' % categorias2)
+
+
+	# função de ativação: degrau bipolar (sinal)
+	def sinal(self, u):
+		return 1 if u >= 0 else -1
+
+
+print('\nX ou T?\n')
+
+amostras = [[1,-1,-1,-1,1,-1,1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,1,-1,1,-1,-1,-1,1],
+			[1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1]]
+
+saidas = [1, -1]
+
+testes = [[1,1,1,1,1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1,-1,-1,1,-1,-1],
+		  [1,-1,-1,-1,1,-1,1,-1,1,-1,-1,-1,1,-1,-1,-1,1,-1,1,-1,1,-1,-1,-1,1]]
+
+
+rede = Perceptron(amostras=amostras, saidas=saidas,	
+						taxa_aprendizado=0.7, epocas=1000)
+
+rede.treinar()
+
+#Resultado
+for teste in testes:
+	rede.testar(teste, 'T', 'X')
